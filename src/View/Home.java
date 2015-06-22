1a1a60de88ec9e8e9702aea44924e9ab0f92435c
AT <sachin.a.perera@gmail.com>
2015-06-22 15:06:26 +0530

    sf

--- src/View/Home.java
+++ src/View/Home.java
@@ -148,7 +148,8 @@ public class Home extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        new stock_management().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
