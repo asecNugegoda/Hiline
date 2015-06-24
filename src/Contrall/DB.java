4f35cac85847201231ab383c3aea2bea55524c25
SjSlash <jsprincesj@gmail.com>
2015-06-24 09:36:13 +0530

    code1
    p1

--- src/Contrall/DB.java
+++ src/Contrall/DB.java
@@ -6,10 +6,23 @@
 
 package Contrall;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Sj
  */
 public class DB {
+    public static Connection con()throws Exception{
+      
+               Class.forName("com.mysql.jdbc.Driver");
+               Connection c =DriverManager.getConnection("jdbc:mysql://localhost:3306/er","root","123");
+                return c;
+        
     
-}
+      
+    }  
+    }
+    
+
